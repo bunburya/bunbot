954fe1885894eb757368ddb18aab18e0b3322ebc
bunburya <bunburya@tcd.ie>
2013-03-04 22:50:24 +0000

    continuing with refactor

--- bot.py
+++ bot.py
@@ -8,9 +8,14 @@ import config, connect
 
 class MessageData:
     
-    def __init__(self, **kwargs):
-        for key in kwargs:
-            setattr(self, key, kwargs[key])
+    def __init__(self):
+        self.irc_cmd = None
+        self.to = None
+        self.from_nick = None
+        self.from_host = None
+        self.channel = None
+        self.tokens = None
+        self.string = None
 
 
 class Bot:
@@ -37,35 +42,29 @@ class Bot:
         self.cmds = config.CommandLib(self)
         self.conn.connect()
         self.conn.mainloop()
-
-    def add_hook(self, hook_type, key, func):
-        self.hooks[hook_type][key] = func
     
-    def reload_cmds(self):
-        self.cmds = reload(config).CommandLib(self)
+    def on_connect(self, data):
+        """
+        Called once we have connected to and identified with the server.
+        Mainly joins the channels that we want to join at the start.
+        """
+        for chan in self.ident.joins:
+            self.conn.join(chan)
     
-    def handle_privmsg(self, tokens, sender):
-        chan = tokens.pop(0)
-        tokens[0] = tokens[0].strip(':')
-        if tokens[0] == self.ident.nick:
-            tokens.pop(0)
-            is_to_me = True
-        else:
-            is_to_me = False
-        # NB, sender is not the sender's nick but a string in the form
-        # <nick>!<host>
-        nick, host = sender.split('!')        
-        if chan == self.ident.nick:
-            chan = nick
+    def handle_nick_in_use(self, data):
+        self.ident.nick += '_'
+        self.conn.connect()
+    
+    def handle_ping(self, data):
+        self.conn.pong(data.string)
 
-        string = ' '.join(tokens)
-
-        #data = {'channel': chan, 'sender': sender, 'is_to_me': is_to_me,
-        #        'nick': nick, 'host': host}
-        data = MessageData(channel=chan, sender=sender, recip=recip,
-                is_to_me=is_to_me, nick=nick, host=host, tokens=tokens,
-                string=string)
-
+    def handle_join(self, data):
+        if data.from_nick != self.ident.nick:
+            for func in self.cmds.other_join_funcs:
+                func(data)
+    
+    def handle_privmsg(self, data):
+        
         try:
             cmd = tokens[0]
         except IndexError:
@@ -87,19 +86,64 @@ class Bot:
                 if groups:
                     self.cmds.regex_funcs[pattern](groups)
         
-    def handle_other_nick(self, tokens, changer):
-        new_nick = tokens.pop().strip(':')
-        old_nick, host = changer.split('!')
-        data = {'old': changer, 'nick': new_nick, 'host': host}
-        for func in self.cmds.other_nick_funcs:
-            func(data)
+    def handle_nick(self, data):
+        if data.from_nick != self.ident.nick:
+            self.hooks['other_nick_change'][''](data)
+    
+    def parse(self, line):
+        if not line:
+            # empty line; this should throw up an error.
+            return
+        line = line.strip('\r\n')
+        tokens = line.split()
+        if tokens[0].startswith(':'):
+            prefix = tokens.pop(0)[1:].strip(':')
+        else:
+            prefix = ''
+        
+        cmd = tokens.pop(0)
 
-    def handle_other_join(self, tokens, joiner):
-        chan = tokens.pop()
-        nick, host = joiner.split('!')
-        data = {'channel': chan, 'joiner': joiner, 'nick': nick, 'host': host}
-        for func in self.cmds.other_join_funcs:
-            func(data)
+        # Maybe best to create MessageData here??
+        data = MessageData()
+        data.irc_cmd = cmd
+        if prefix:
+            data.from_nick, data.from_host = prefix.split('!')
+
+        if cmd == '433':    # nick already in use
+            handler = self.handle_nick_in_use
+        elif cmd == '376':    # end of MOTD
+            handler = self.on_connect
+        elif cmd == 'PING':
+            handler = self.handle_ping
+        elif cmd == 'ERROR':
+            handler = self.handle_errors
+        elif cmd == 'JOIN':
+            data.to = tokens.pop(0)
+            handler = self.handle_join
+        elif cmd == 'PRIVMSG':
+            data.to = tokens.pop(0)
+            handler = self.handle_privmsg
+        elif cmd == 'NICK':
+            data.to = tokens.pop(0)
+            handler = self.handle_nick
+        
+        data.tokens = tokens
+        data.string = ' '.join(tokens)
+        handler(data)
+
+    def add_hook(self, hook_type, key, func):
+        # empty key will always fire
+        self.hooks[hook_type][key] = func
+    
+    def fire_hook(self, hook_type, key, data):
+        try:
+            self.hooks[hook_type][key](data)
+        except KeyError:
+            # should probably do something here
+            pass
+    
+    def reload_cmds(self):
+        self.cmds = reload(config).CommandLib(self)
         
 
 if __name__ == '__main__':
--- connect.py
+++ connect.py
@@ -9,29 +9,15 @@ class IRCConn:
     
     def __init__(self, bot):
         self._bot = bot
-        i = bot.ident
-        self._ident = i.ident
-        self._serv = i.serv
-        self._host = i.host
-        self._name = i.name
-        self._nick = i.nick
-        self._join_first = i.joins
+        self._id = bot.ident
         self._port = 6667
     
     def connect(self):
         self._sock = socket(AF_INET, SOCK_STREAM)
-        self._sock.connect((self._host, self._port))
-        self._send('USER {} {} {} :{}'.format(self._ident, self._host, self._serv, self._name))
-        self.nick(self._nick)
+        self._sock.connect((self._id.host, self._port))
+        self._send('USER {} {} {} :{}'.format(self._id.ident, self._id.host, self._id.serv, self._id.name))
+        self.nick(self._id.nick)
         # wait until we have received the MOTD in full before proceeding
-
-    def on_connect(self):
-        """
-        Called once we have connected to and identified with the server.
-        Mainly joins the channels that we want to join at the start.
-        """
-        for chan in self._join_first:
-            self.join(chan)
     
     def join(self, chan):
         self._send('JOIN {}'.format(chan))
@@ -77,43 +63,11 @@ class IRCConn:
             return ''
         print('received:', line)
         return line
-            
-    def parse(self, line):
-        if not line:
-            # empty line; this should throw up an error.
-            return
-        line = line.strip('\r\n')
-        tokens = line.split()
-        if tokens[0].startswith(':'):
-            prefix = tokens.pop(0)[1:].strip(':')
-        else:
-            prefix = ''
-        
-        cmd = tokens.pop(0)
-
-        # Maybe best to create MessageData here??
-
-        if cmd == '433':    # nick already in use
-            self._nick += '_'
-            self.nick(self._nick)
-        elif cmd == '376':    # end of MOTD
-            self.on_connect()
-        elif cmd == 'PING':
-            self.pong(' '.join(tokens))
-        elif cmd == 'ERROR':
-            self.handle_error(tokens)
-        elif cmd == 'JOIN':
-            if prefix.split('!')[0] != self.nick:
-                self._bot.handle_other_join(tokens, prefix)
-        elif cmd == 'PRIVMSG':
-            self._bot.handle_privmsg(tokens, prefix)
-        elif cmd == 'NICK':
-            self._bot.handle_other_nick(tokens, prefix)
 
     def mainloop(self):
         while True:
             line = self.receive()
-            self.parse(line)
+            self._bot.parse(line)
 
     def handle_encoding_error(self):
         print('encoding error encountered.')
