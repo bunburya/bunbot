b7ba35cfe7db355e6c631573c0b6d6916f98495f
bunburya <bunburya@tcd.ie>
2013-10-12 19:11:52 +0100

    fixing issues with grabbing titles where url redirects to non-ascii url

--- plugins/url.py
+++ plugins/url.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, Request, build_opener, HTTPRedirectHandler
 from urllib.parse import quote
 from urllib.error import URLError
 from html.parser import HTMLParseError
@@ -6,7 +6,35 @@ import re
 
 from bs4 import BeautifulSoup
 
+ascii_chars = ''.join((chr(i) for i in range(128)))
+
+def quote_url(url):
+    return quote(url, safe=ascii_chars)
+
+class RedirectHandler(HTTPRedirectHandler):
+    
+    def _handle_redirect(self, req, fp, code, msg, headers):
+        url_unescaped = headers.get('Location')
+        new_url = quote_url(url_unescaped)
+        headers.replace_header('Location', new_url)
+        result = HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+        return result
+                                                    
+    def http_error_301(self, *args):
+        return self._handle_redirect(*args)
+                                                                    
+    def http_error_302(self, *args):
+        return self._handle_redirect(*args)
+                                                                                    
+    def http_error_303(self, *args):
+        return self._handle_redirect(*args)
+                                                                                                    
+    def http_error_307(self, *args):
+        return self._handle_redirect(*args)
+
 class Plugin:
+
+    req_header = {'User-Agent': 'bunbot IRC bot'}
     
     # This regex is borrowed from Django, with some groups added in
     url_regex = re.compile(
@@ -28,6 +56,10 @@ class Plugin:
                 {'type': 'privmsg_re', 'key': self.url_regex, 'func': self.title}
                 ]
 
+    def fetch_url(self, url):
+        req = Request(url, headers=self.req_header)
+        opener = build_opener(RedirectHandler())
+        return opener.open(req)
 
     def parse_url(self, data):
         """To be called before the other functions.
@@ -35,7 +67,7 @@ class Plugin:
         by other functions."""
         match = data.regex_match
         url, domain, ipv4, ipv6, port, filepath = match.groups()
-        url = quote(url, safe=':/%')    # To handle unicode chars in the URL
+        url = quote_url(url)    # To handle unicode chars in the URL
         self.url_data = {}
         self.url_data['url'] = url
         self.url_data['domain'] = domain
@@ -45,9 +77,11 @@ class Plugin:
         self.url_data['filepath'] = filepath
 
     def title(self, data):
+        print('title called')
         try:
-            html = urlopen(self.url_data['url']).readall()
-        except (URLError):
+            html = self.fetch_url(self.url_data['url']).readall()
+        except URLError as e:
+            raise e
             return
         try:
             soup = BeautifulSoup(html)
