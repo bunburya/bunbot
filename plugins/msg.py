8651c08f1dd333375c30bc0c846ac89e5e26778a
bunburya <bunburya@tcd.ie>
2014-03-06 04:03:00 +0000

    fixed multiple notification bug in msg

--- plugins/msg.py
+++ plugins/msg.py
@@ -39,7 +39,7 @@ class Plugin:
     def _save_msgs(self, msgs):
         with open(self.msg_file, 'w') as f:
             dump(msgs, f)
-    
+
     def get_msg_file(self):
         plugin_dir = self.handler.plugin_dir
         serv = self.bot.ident.host
@@ -68,9 +68,21 @@ class Plugin:
 
     def check_for_msgs(self, data):
         msgs = self._load_msgs()
-        if data.from_nick.lower() in msgs:
-            self.conn.say('You have messages. Type "!msg" to view them.',
-                    data.from_nick)
+        user_msgs = msgs.get(data.from_nick.lower(), None)
+        if not user_msgs:
+            return
+        total_count = len(user_msgs)
+        new = list(filter(lambda m: not m['notified'], user_msgs))
+        new_count = len(new)
+        if new:
+            self.conn.say('You have {} new messages ({} total). '
+                'Type "!msg" to view them.'.format(new_count, total_count),
+                data.from_nick)
+            for m in new:
+                m['notified'] = True
+            self._save_msgs(msgs)
+
+
 
     def view_msgs(self, data):
         msgs = self._load_msgs()
@@ -78,21 +90,30 @@ class Plugin:
         if user_msgs is None:
             self.conn.say('No messages.', data.from_nick)
         else:
-            for sender, msg, time in user_msgs:
-                self.conn.say('<{}> {} (sent at {})'.format(sender, msg, time),
+            for msg in user_msgs:
+                sender = msg['from']
+                text = msg['text']
+                time = msg['time']
+                self.conn.say('<{}> {} (sent at {})'.format(sender, text, time),
                         data.from_nick)
         self._save_msgs(msgs)
 
     def send_msg(self, data):
         _from = data.from_nick
         to = data.tokens[0].lower()
-        msg = ' '.join(data.tokens[1:])
+        text = ' '.join(data.tokens[1:])
         time = asctime()
         msgs = self._load_msgs()
+        msg = {
+                'from': _from,
+                'text': text,
+                'time': time,
+                'notified': False
+                }
         if to in msgs:
-            msgs[to].append((_from, msg, time))
+            msgs[to].append(msg)
         else:
-            msgs[to] = [(_from, msg, time)]
+            msgs[to] = [msg]
         self._save_msgs(msgs)
         self.conn.say('Message sent.', data.to)
     
